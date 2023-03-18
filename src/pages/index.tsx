655f10c45f5f2b8a93c253961e0326088e325faf
Jonatas <jonatasgs7@gmail.com>
2023-03-18 12:57:34 -0300

    Inserção das tags meta e configurações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Dr. Maurício Torres</title>
-        <meta name="description" content="Site Pessoal do Dr. Maurício Torres" />
+        <title>Dr. Maurício Torres - Ginecologia e Terapia Hormonal</title>
+        <meta name="description" content="Site Pessoal do Dr. Maurício Torres, especialista em Ginecologia e Terapia Hormonal" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#436681" />
         <link rel="icon" href="/img/favicon.png" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://mauriciotorres.com.br/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Dr. Maurício Torres - Ginecologia e Terapia Hormonal" />
+        <meta property="og:image" content="https://mauriciotorres.com.br/img/arte_drmaurcio.png" />
+        <meta property="og:url" content="https://mauriciotorres.com.br/" />
       </Head>
 
       <Header />
